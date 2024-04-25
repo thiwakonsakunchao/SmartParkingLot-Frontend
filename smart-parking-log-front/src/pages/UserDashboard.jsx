c5f1d785c2da720c453d1c9a406209c9bce91e1a
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2024-04-25 20:55:48 +0700

    test condition user dashboard

--- smart-parking-log-front/src/pages/UserDashboard.jsx
+++ smart-parking-log-front/src/pages/UserDashboard.jsx
@@ -1,12 +1,47 @@
 import CountCar from "../components/CountCar"
 import CameraCCTV from "../components/CameraCCTV"
 import CameraPlate from "../components/CameraPlate"
+import PlatePass from "../components/PlatePass";
+import PlateNoPass from "../components/PlateNoPass";
+import { useState, useEffect} from "react";
+import axios from "axios";
 
 function UserDashboard(){
     
+    const [status, setStatus] = useState([]);
+    const [haveCars, setHaveCars] = useState(false);
+    const [resultScan, setResultScan] = useState(false);
+
+
+    const fetchStatus = async () =>{
+        try {
+            const response = await axios.get('http://localhost:3000/events')
+            setStatus(response.data[0]);
+            setHaveCars(status.haveCars);
+            setResultScan(status.resultScan);
+        } catch (error) {
+            console.log('error', error)
+        }
+
+    }
+
+    useEffect(()=>{
+        fetchStatus()
+        console.log(status);
+     }, [status])
+
+
     return(
         <>
-           <div className="grid grid-cols-2 h-full">
+
+            {haveCars ? (
+                resultScan ? (
+                    <PlatePass />
+                ) : (
+                    <PlateNoPass />
+                )
+            ) : (
+                <div className="grid grid-cols-2 h-full">
                 <div className="left-side flex justify-center items-center ">
                     <CountCar />
                 </div>
@@ -16,7 +51,9 @@ function UserDashboard(){
                         <CameraPlate />
                      </div>
                 </div>
-           </div>
+                </div>
+            )}
+
         </>
 
     )
