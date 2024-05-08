bcdefd0ccb943cc829ca0e381ec32ad3c0d6114e
thiwakonsakunchao <thiwakon_sakunchao@cmu.ac.th>
2024-05-08 15:15:38 +0700

    make can't go to login page when already logged in

--- smart-parking-lot-front/src/pages/LoginPage.jsx
+++ smart-parking-lot-front/src/pages/LoginPage.jsx
@@ -1,27 +1,34 @@
 import axios from "axios";
-
+import { useEffect } from "react";
 
 function LoginPage() {
 
+    useEffect(() => {
+        const authToken = localStorage.getItem("token");
+        if (authToken) {
+            // Redirect to another page if token is present
+            window.location = "/admin";
+        }
+    }, []);
+
     const login = async () => {
         try {
-            const username = document.querySelector('input[name=username]').value
-        const password = document.querySelector('input[name=password]').value
-        const response = await axios.post('http://localhost:8000/api/login', {
-            username,
-            password
-        })
+            const username = document.querySelector('input[name=username]').value;
+            const password = document.querySelector('input[name=password]').value;
+            const response = await axios.post('http://localhost:8000/api/login', {
+                username,
+                password
+            });
 
-        localStorage.setItem('token', response.data.token)
-        window.location = '/admin'
+            localStorage.setItem('token', response.data.token);
+            window.location = '/admin';
 
-        console.log(username,password);
-        console.log(response.data.token);
+            console.log(username, password);
+            console.log(response.data.token);
         } catch (error) {
-            console.log('error', error)
+            console.log('error', error);
         }
-    }
-
+    };
 
     return (
         <>
@@ -31,19 +38,18 @@ function LoginPage() {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 ">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-[#2A3990] md:text-2xl ">
-                                Sign in to going admin page
+                                Sign in to go to the admin page
                             </h1>
 
-                            
-                                <div>
-                                    <label htmlFor="username" className="block mb-2 text-sm font-medium text-[#2A3990] ">Username</label>
-                                    <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="username" required="" />
-                                </div>
-                                <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-[#2A3990] ">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
-                                </div>
-                               <button  className="w-full text-white bg-[#2A3990] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={login}>Sign in</button>
+                            <div>
+                                <label htmlFor="username" className="block mb-2 text-sm font-medium text-[#2A3990] ">Username</label>
+                                <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="username" required="" />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-[#2A3990] ">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
+                            </div>
+                            <button className="w-full text-white bg-[#2A3990] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={login}>Sign in</button>
 
                         </div>
                     </div>
